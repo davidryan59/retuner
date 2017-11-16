2ab2bf97bcf777db8f950d2e3d8a5a9a9a8f39ef
David Ryan <davidryan1998@hotmail.com>
2017-11-16 08:54:12 +0000

    Text added to keys

--- client/src/views/draw_instrument_keys.js
+++ client/src/views/draw_instrument_keys.js
@@ -10,16 +10,23 @@ const drawInstrumentKeys = (state) => {
   for (const key of state.keys) {
     if (key.physicsSwitchedOn) {
       const x = key.location.x * coordFactor
-      const y = key.location.y * coordFactor
+      const y = canvasHeight - key.location.y * coordFactor
       const r = key.location.r * radiusFactor
       // context.fillStyle = key.getBgColour(state, key)
       context.fillStyle = key.bgColour(state, key)
       context.strokeStyle = 'rgba(20, 20, 20, 0.6)'
       context.lineWidth = 4
       context.beginPath()
-      context.arc(x, canvasHeight-y, r, 0, 2*Math.PI);
+      context.arc(x, y, r, 0, 2*Math.PI);
       context.stroke();
       context.fill()
+      // Draw button labels
+      context.fillStyle = 'rgb(0, 0, 0)'
+      let theText = key.keyboardCode
+      if (key.transposes) {
+        theText = `${key.transposes.text} ${theText}`
+      }
+      context.fillText(theText, x - 0.6*r, y + 0.1*r)
     }
   }
 }
