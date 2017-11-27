cf83cd01e9d3b9d89815f6be304d026f8417f96a
David Ryan <davidryan1998@hotmail.com>
2017-11-27 09:38:14 +0700

    Started app parameters file

---
+++ client/src/_params/app_params.js
@@ -0,0 +1,10 @@
+const setupAppParams = (state) => {
+
+  state.params = {}
+  const stateParams = state.params
+  stateParams.loopsPerFrame = 2
+  console.log("App parameters set up")
+
+}
+
+export default setupAppParams
