e214fbd34a1d321b491e60c7acc49c62aa796eba
David Ryan <davidryan1998@hotmail.com>
2017-11-30 09:54:23 +0700

    Starting v1.0.5. Switched from large to medium logo, for speed.

--- client/src/_params/current_version.js
+++ client/src/_params/current_version.js
@@ -1,7 +1,7 @@
 const setupCurrentVersion = (state) => {
 
   state.version = {}
-  state.version.number = "v1.0.4"
+  state.version.number = "v1.0.5"
   state.version.date = "30th Nov 2017"
   console.log(`Currently on ${state.version.number} (${state.version.date})`)
 
