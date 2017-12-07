bc0b9de58c08d3660b72b71ab8d984d932e79f4e
David Ryan <davidryan1998@hotmail.com>
2017-12-07 10:58:14 +0700

    Comma info now fraction objects. Multiply function also takes a power.

--- client/src/calculations/check_prime_info.js
+++ client/src/calculations/check_prime_info.js
@@ -1,5 +1,6 @@
 import extractAllPrimeFactors from './extract_all_prime_factors'
 import commaRcnDr from '../maths/commas/comma_rcn_dr'
+import fractionToObject from '../maths/fraction_object/fraction_to_object'
 
 // const extractAllPrimeFactors = require('./extract_all_prime_factors')
 // const commaRcnDr = require()'../maths/commas/rcn_dr')
@@ -27,7 +28,9 @@ const checkPrimeInfo = (state) => {
         const [a, b] = commaRcnDr(prime)
         const num = prime * (2 ** Math.max(0, a)) * (3 ** Math.max(0, b))
         const denom = (2 ** Math.max(0, -a)) * (3 ** Math.max(0, -b))
-        commaObject[prime] = [num, denom, prime, a, b]
+        const fractObj = fractionToObject(num, denom)
+        // commaObject[prime] = [num, denom, prime, a, b]
+        commaObject[prime] = fractObj
       }
     }
     state.prime.commas = commaObject
