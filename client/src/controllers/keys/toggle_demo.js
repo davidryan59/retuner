abd2096aad2c8f8e32f36e9811e0197b9a3ba0c0
David Ryan <davidryan1998@hotmail.com>
2017-12-11 21:41:23 +0700

    Start Demo stubbed

--- client/src/controllers/keys/toggle_demo.js
+++ client/src/controllers/keys/toggle_demo.js
@@ -1,11 +1,18 @@
+import startDemo from '../../demos/start_demo'
+
 const toggleDemo = (state, key) => {
   // On starting a demo, need a loading activity, as well as toggle on
   // On finishing, just need to toggle off
-  const playing = state.demo.playing
-  state.demo.playing = (playing) ? false : true
-  console.log(`Demo toggled to ${state.demo.playing} (NOT YET IMPLEMENTED)`)
 
+  // Toggle state
+  const stateDemo = state.demo
+  stateDemo.playing = (stateDemo.playing) ? false : true  
+  console.log(`Demo toggled to ${state.demo.playing}`)
 
+  // Start demo if needed
+  if (stateDemo.playing) {
+    startDemo(state)
+  }
 }
 
 export default toggleDemo
---
+++ client/src/demos/start_demo.js
@@ -0,0 +1,8 @@
+const startDemo = (state, key) => {
+
+  console.log(`Demo started (NOT YET IMPLEMENTED)`)
+
+
+}
+
+export default startDemo
