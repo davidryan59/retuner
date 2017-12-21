6b27a4f55dc45e4cccbd2d39c12e806a0262f64c
David Ryan <davidryan1998@hotmail.com>
2017-12-21 11:40:24 +0700

    Starting v1.3.2

--- client/src/_params/current_version.js
+++ client/src/_params/current_version.js
@@ -1,6 +1,6 @@
 const setupCurrentVersion = (state) => {
 
-  state.version.number = "v1.3.1"
+  state.version.number = "v1.3.2"
   state.version.date = "21st Dec 2017"
   console.log(`Currently on ${state.version.number} (${state.version.date})`)
 
