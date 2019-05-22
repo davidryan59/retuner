e9c20cddc81ddd9a8427a62941474f3ce759548e
David Ryan <davidryan1998@hotmail.com>
2019-05-22 12:50:44 +0700

    1.3.5

--- client/src/_params/current_version.js
+++ client/src/_params/current_version.js
@@ -1,6 +1,6 @@
 const setupCurrentVersion = (state) => {
 
-  state.version.number = "v1.3.4"
+  state.version.number = "v1.3.5"
   state.version.date = "22nd May 2019"
   console.log(`Currently on ${state.version.number} (${state.version.date})`)
 
