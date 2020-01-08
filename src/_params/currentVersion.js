622e71b2c4a3a69af82e280ba2f6a3866e73f9ca
David Ryan <davidryan1998@hotmail.com>
2020-01-08 14:18:27 +0700

    Switched Firebase from retuner-bdf49.web.app to re-tuner.web.app

--- src/_params/currentVersion.js
+++ src/_params/currentVersion.js
@@ -1,7 +1,7 @@
 const setupCurrentVersion = state => {
 
-  state.version.number = "v1.3.8"
-  state.version.date = "20th December 2019"
+  state.version.number = "v1.3.9"
+  state.version.date = "8th January 2020"
   console.log(`Currently on ${state.version.number} (${state.version.date})`)
 
 }
